7cc9f30f4278d952695b5010800a99d8692708b5
nabilo1989 <nabilo.majid@gmail.com>
2025-05-08 19:43:37 +0330

    run base 005 end step1

--- phonebook_project/urls.py
+++ phonebook_project/urls.py
@@ -15,41 +15,81 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib.auth import views as auth_views
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#
+#     # URL‌های احراز هویت
+#     path('members/password_reset/',
+#     auth_views.PasswordResetView.as_view(
+#                template_name='members/password_reset.html',
+#                email_template_name='members/password_reset_email.html',
+#                subject_template_name='members/password_reset_subject.txt',
+#                success_url='members/password_reset'  # اضافه کردن این خط
+#     ),
+#            name='password_reset'),
+#
+#     # path('members/password_reset/done/',
+#     # auth_views.PasswordResetDoneView.as_view(
+#     #            template_name='members/password_reset_done.html'
+#     # ),
+#     #        name='password_reset_done'),
+#
+#     path('members/reset/<uidb64>/<token>/',
+#            auth_views.PasswordResetConfirmView.as_view(
+#                template_name='members/password_reset_confirm.html'
+#            ),
+#            name='password_reset_confirm'),
+#
+#     path('members/reset/done/',
+#            auth_views.PasswordResetCompleteView.as_view(
+#                template_name='members/password_reset_complete.html'
+#            ),
+#            name='password_reset_complete'),
+#     path('', include('phonebook.urls')),
+#     path('members/', include('members.urls')),#,namespace='members'
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # URL‌های احراز هویت
     path('members/password_reset/',
     auth_views.PasswordResetView.as_view(
-               template_name='members/password_reset.html',
-               email_template_name='members/password_reset_email.html',
-               subject_template_name='members/password_reset_subject.txt',
-               success_url='password_reset_done'  # اضافه کردن این خط
+        template_name='members/password_reset.html',
+        email_template_name='members/password_reset_email.html',
+        subject_template_name='members/password_reset_subject.txt',
+        success_url='/members/password_reset/done/'
     ),
-           name='password_reset'),
+    name='password_reset'),
 
     path('members/password_reset/done/',
-    auth_views.PasswordResetDoneView.as_view(
-               template_name='members/password_reset_done.html'
-    ),
-           name='password_reset_done'),
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='members/password_reset_done.html'
+        ),
+        name='password_reset_done'),  # حذف کامنت این خط
 
     path('members/reset/<uidb64>/<token>/',
-           auth_views.PasswordResetConfirmView.as_view(
-               template_name='members/password_reset_confirm.html'
-           ),
-           name='password_reset_confirm'),
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='members/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'),
 
     path('members/reset/done/',
-           auth_views.PasswordResetCompleteView.as_view(
-               template_name='members/password_reset_complete.html'
-           ),
-           name='password_reset_complete'),
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='members/password_reset_complete.html'
+        ),
+        name='password_reset_complete'),
+
     path('', include('phonebook.urls')),
-    path('members/', include('members.urls')),#,namespace='members'
+    path('members/', include('members.urls')),  # namespace اختیاری است
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
